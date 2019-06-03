c97202b07f19ff0fa3d554d8fe813075afa9d19c
Christophe Monniez <moc@odoo.com>
2019-06-03 16:36:50 +0200

    m

--- runbot/models/build.py
+++ runbot/models/build.py
@@ -917,7 +917,7 @@ class runbot_build(models.Model):
     def _save_dump(self):
         """ If a dump exists in the build dump dir, save it on the branch """
         self.ensure_one()
-        dumps = glob.glob(self._path('dump/%s-*.sql.gz' % self.dest))
+        dumps = glob.glob(self._path('dump/%s-*.tar.gz' % self.dest))
         if dumps:
             with open(dumps[0], 'rb') as dump_file:
                 data = dump_file.read()
