4443337bd0f5d3bd7b1d2339da9c5d54641e621e
Christophe Monniez <moc@odoo.com>
2019-06-05 13:25:16 +0200

    WIP

--- runbot_migrate/controllers/frontend.py
+++ runbot_migrate/controllers/frontend.py
@@ -8,12 +8,12 @@ class RunbotMigrate(Controller):
     @route(['/runbot/branch/<int:branch_id>/lastdump'], website=True, auth='public', type='http')
     def branch_dump(self, branch_id=None, **kwargs):
         """ Download last branch dump if it exists """
-        domain = [('id', '=', branch_id), ('dump_id', '!=', False)]
+        domain = [('id', '=', branch_id), ('last_dump_id', '!=', False)]
         branch = request.env['runbot.branch'].search(domain, limit=1)
         if branch:
             headers = [
                 ('Content-Type', 'application/gzip'),
-                ('Content-Disposition',  'attachment; filename="%s"' % branch.dump_id.datas_fname),
+                ('Content-Disposition',  'attachment; filename="%s"' % branch.last_dump_id.datas_fname),
             ]
             dump_data = base64.decodebytes(branch.dump_id.datas)
             return request.make_response(dump_data, headers)
